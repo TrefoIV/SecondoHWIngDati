ac92f0e2d95aca517e08bab7fe9bc02640dffc66
Sue <sus.caroppo@stud.uniroma3.it>
2021-12-01 10:33:45 +0100

    estratti dati docenti bocconi

--- WebExtraction/WebExtraction/spiders/DettagliDocenteR3.py
+++ WebExtraction/WebExtraction/spiders/DettagliDocenteR3.py
@@ -2,22 +2,37 @@ import scrapy
 import json
 
 class ToScrapeSpiderXPath(scrapy.Spider):
-    name = 'dettagliDocenti'
+    name = 'dettagliDocentiR3'
     def start_requests(self):
         f = open('docenti.json')
         data = json.load(f)
         self.start_urls = []
         for i in data:
-            start_urls.append('https://www.uniroma3.it' + i['link'])
-        print(start_urls)
+            self.start_urls.append('https://www.uniroma3.it' + i['link'] +'/insegnamenti/')
+        #print(start_urls)
         return super().start_requests()
 
     def parse(self, response):
         nome = response.xpath('//*[@id="main"]/h1/text()')
         caratteristiche = response.xpath('//div[@class="boxPersonaDetailsWrap"]/table/tbody/tr/td')
-        yield {
+        profProperty = {
             'nome' : nome.extract_first(),
             'qualifica': caratteristiche.xpath('./strong[contains(.,"Qualifica")]/ancestor-or-self::td/following-sibling::td/text()').extract_first(),
             'SSD': caratteristiche.xpath('./strong[contains(.,"Settore Scientifico Disciplinare")]/ancestor-or-self::td/following-sibling::td/text()').extract_first(),
             'email' : caratteristiche.xpath('.//strong[contains(.,"Email")]/ancestor-or-self::td/following-sibling::td/text()').extract_first(),
+            'email' : caratteristiche.xpath('./strong[contains(.,"Email")]/ancestor-or-self::td/following-sibling::td/text()').extract_first(),
+            'link al curriculum' : caratteristiche.xpath('./a[contains(.,"Curriculum")]/@href').extract_first(),
             }
+     
+        
+        
+        profProperty['corsi'] = []
+        for corso in response.xpath('//main[@id="main"]/h2[1]/following-sibling::strong/text()'):
+            profProperty['corsi'].append(corso.get())
+
+        ##profPropertyJson = json.dump(profProperty)
+        with open('dettagliDocenti2.json', 'a') as fp:
+            json.dump(profProperty, fp)
+            fp.write('\n')
+        #print(profProperty)
+        #yield({profProperty})
