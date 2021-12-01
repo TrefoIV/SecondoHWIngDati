6f6432434b72be16f742c1296f36b50e04c038df
TrefoIV <57830785+TrefoIV@users.noreply.github.com>
2021-12-01 12:46:28 +0100

    Completed WikiCompaniesSpider

--- WebExtraction/WebExtraction/spiders/WikiCompaniesSpider.py
+++ WebExtraction/WebExtraction/spiders/WikiCompaniesSpider.py
@@ -14,32 +14,51 @@ class WikiCompaniesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parseCompaniesIndex)
 
     def parseCompaniesIndex(self, response):
+
         for span in response.xpath("//span[@class='flagicon']"):
+
             industrial_sector = span.xpath("normalize-space(../../div/descendant::b/a/text())").get()
             if industrial_sector == "":
                 industrial_sector = span.xpath("normalize-space(../../p/b/a/text())").get()
 
-            for hrefSelector in span.xpath("following::small[1]/a/@href"):
+            for index, hrefSelector in enumerate(span.xpath("following::small[1]/a/@href")):
+                name = span.xpath(f"following::small[1]/a[{index+1}]/text()").get()
                 link = response.urljoin(hrefSelector.get())
-                yield scrapy.Request(url = "https://en.wikipedia.org/wiki/Qantas", callback = self.parseCompany, cb_kwargs = {"industrial_sector" : industrial_sector})
-                break
-            break
+                yield scrapy.Request(url = link, callback = self.parseCompany, cb_kwargs = {"industrial_sector" : industrial_sector, "name" : name})
+            
 
-    def parseCompany(self, response, industrial_sector):
-        attributes = ["Founded", "Revenue"]
-        for attr in attributes:
-            #row  = response.xpath(f"//tr/descendant::*[text() = '{attr}']")
-            #row = row.xpath("parent::tr/td")
-            #response.xpath("//tr/descendant::*[text() = 'Founded']/parent::tr/td")
+    def parseCompany(self, response, industrial_sector, name):
+        attributes = ["Founded", "Revenue", "Key people", "Headquarters", "Total assets", "Employees", "Number of employees", "Website", "Hubs"]
+        company_description = {
+            "name" : name,
+            "industrial_sector" : industrial_sector
+        }
+
+        for attr in attributes:          
             attr_value = ""
-            for text in response.xpath(f"//tr/descendant::*[text() = '{attr}']/ancestor::tr/td//text()"):
-                stringa = text.get()
-                stringa = normalize("NFKC", stringa )    #Trasforma eventuali caratteri speciali unicode contenuti nel testo
-                stringa = re.sub("\[[1-9]*\]", "", stringa) #Rimuove eventuali riferimenti a fonti di wikipedia
-                print(stringa)
-                attr_value += stringa
-                print(attr_value)
-            print(f"{attr} : {attr_value}")
+            list_attr = response.xpath(f"//tr/descendant::*[text() = '{attr}']/ancestor::tr//ul") != []
+            if list_attr:
+                attr_value = []
+                for li in response.xpath(f"//tr/descendant::*[text() = '{attr}']/ancestor::tr//ul/li"):
+                    attr_value.append(self.buildAttribute(li))
+            else:
+                td = response.xpath(f"//tr/descendant::*[text() = '{attr}']/ancestor::tr/td")
+                attr_value += self.buildAttribute(td)
+
+            if attr_value == "" or attr_value == []:    #L'attributo non ha valori, metto un valore nullo
+                attr_value = None
+            
+            company_description[attr] = attr_value
+        yield company_description
+
+    def buildAttribute(self, node):
+        attr_value = ""
+        for text in node.xpath(f"descendant-or-self::*//text()"):
+            stringa = text.get()
+            stringa = normalize("NFKC", stringa )    #Trasforma eventuali caratteri speciali unicode contenuti nel testo
+            stringa = re.sub("\[[1-9]*\]", "", stringa) #Rimuove eventuali riferimenti a fonti di wikipedia
+            attr_value += stringa
+        return attr_value
 
     def parse(self, response):
         page = response.url.split("/")[-2]
