0b5b038e2429e37fb3eea4cdcc740955b33a9a00
Fyu11 <silvia.montors@gmail.com>
2021-12-03 19:36:44 +0100

    Aggiunta attributo "rank" per ogni istanza e conteggio valori nulli

--- WebExtraction/WebExtraction/spiders/DettagliAzienda.py
+++ WebExtraction/WebExtraction/spiders/DettagliAzienda.py
@@ -4,9 +4,10 @@ import json
 
 class QuotesSpider(scrapy.Spider):
     name = 'dettagliAziende'
+    total=0
 
     def start_requests(self):
-        f = open('aziende.json')
+        f = open('aziende.json', encoding="utf8")
         data = json.load(f)
         self.start_urls = []
         for i in data:
@@ -15,11 +16,21 @@ class QuotesSpider(scrapy.Spider):
         return super().start_requests()
 
     def parse(self, response):
-        #for dettagli in response.xpath('//div[@class="table-container"]'):
-            yield {
-                'name' : response.xpath('//div[@class="company-title-container"]/div[@class="company-name"]/text()').extract_first(),
-                'code' : response.xpath('//div[@class="company-title-container"]/div[@class="company-code"]/text()').extract_first(),
-                'country' : response.xpath('//div[@class="col-lg-6"]/div/div/div/a/span/text()').extract_first(),
-                'marketcap' : response.xpath('//div[@class="col-lg-6"]/div/div[2]/div[1]/text()').extract_first(),
-                'competitor': response.xpath('//table/tbody/*//div[@class="company-name"]/text()').getall(),
-            }
+
+        proprieta={
+        'name' : response.xpath('//div[@class="company-title-container"]/div[@class="company-name"]/text()').extract_first(),
+        'code' : response.xpath('//div[@class="company-title-container"]/div[@class="company-code"]/text()').extract_first(),
+        'rank' : response.xpath('//div[@class="col-lg-6"]/div[1]/div[1]/div[1]/text()').extract_first().replace("#",""),
+        'country' : response.xpath('//div[@class="col-lg-6"]/div/div/div/a/span/text()').extract_first(),
+        'marketcap' : response.xpath('//div[@class="col-lg-6"]/div/div[2]/div[1]/text()').extract_first(),
+        'competitor' : response.xpath('//table/tbody/*//div[@class="company-name"]/text()').getall()
+        }
+
+        yield proprieta
+
+        for attributo in proprieta:
+            if(proprieta[attributo]=="" or proprieta[attributo]==[]):
+                self.total+=1
+
+    def closed(self, reason):
+        print("--- Valori nulli: ", self.total)
