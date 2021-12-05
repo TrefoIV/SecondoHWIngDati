d2e84250a2f0d14438ab30f83d21b7cea017997b
Sue <sus.caroppo@stud.uniroma3.it>
2021-12-05 10:01:40 +0100

    caratteristiche dataset corretti bug

--- WebExtraction/WebExtraction/caratteristicheDatasetDocenti.py
+++ WebExtraction/WebExtraction/caratteristicheDatasetDocenti.py
@@ -7,17 +7,17 @@ class caratteristiche_dataset_docenti(object):
     valNullDipartimentoBocconi = 0
     valNullCorsiBocconi = 0
     with open('WebExtraction/dettagliDocentiBocconi.json', 'r') as fp: 
-        data = json.load(fp)
-    for i in range(len(data)):   
-        if data[i]['nome'] is None:
+        dataBocconi = json.load(fp)
+    for i in range(len(dataBocconi)):   
+        if dataBocconi[i]['nome'] is None:
             valNullNomeBocconi += 1
-        if data[i]['qualifica'] is None:
+        if dataBocconi[i]['qualifica'] is None:
             valNullQualificaBocconi += 1
-        if data[i]['dipartimento'] is None:
+        if dataBocconi[i]['dipartimento'] is None:
             valNullDipartimentoBocconi += 1
-        if data[i]['email'] is None:
+        if dataBocconi[i]['email'] is None:
             valNullEmailBocconi += 1
-        if len(data[i]['corsi']) == 0:
+        if len(dataBocconi[i]['corsi']) == 0:
             valNullCorsiBocconi += 1
 
     caratteristicheBocconi = {
@@ -34,18 +34,18 @@ class caratteristiche_dataset_docenti(object):
     valNullDipartimentoR3 = 0
     valNullCorsiR3 = 0
     with open('WebExtraction/dettagliDocentiR3.json', 'r', encoding="utf8") as fp: 
-        data = json.load(fp)
+        dataR3 = json.load(fp)
         #print(data)
-    for i in range(len(data)): 
-        if data[i]['nome'] is None:
+    for i in range(len(dataR3)): 
+        if dataR3[i]['nome'] is None:
             valNullNomeR3 += 1
-        if data[i]['qualifica'] is None:
+        if dataR3[i]['qualifica'] is None:
             valNullQualificaR3 += 1
-        if data[i]['dipartimento'] is None:
+        if dataR3[i]['dipartimento'] is None:
             valNullDipartimentoR3 += 1
-        if data[i]['email'] is None:
+        if dataR3[i]['email'] is None:
             valNullEmailR3 += 1
-        if len(data[i]['corsi']) == 0:
+        if len(dataR3[i]['corsi']) == 0:
             valNullCorsiR3 += 1
 
     caratteristicheR3 = {
@@ -56,5 +56,37 @@ class caratteristiche_dataset_docenti(object):
         'valNullCorsiR3' : valNullCorsiR3
     }
 
-    print(caratteristicheBocconi)
-    print(caratteristicheR3)
+    percentualiValoriNulli ={
+        'percValNullNomiR3' : (valNullNomeR3/len(dataR3))*100,
+        'valNullNomeBocconi' : (valNullNomeBocconi/len(dataBocconi))*100,
+        'percValNullQualificaR3' : (valNullQualificaR3/len(dataR3))*100,
+        'valNullQualificaBocconi' : (valNullQualificaBocconi/len(dataBocconi))*100,
+        'valNullDipartimentoR3' : (valNullDipartimentoR3/len(dataR3))*100,
+        'valNullDipartimentoBocconi' : (valNullDipartimentoBocconi/len(dataBocconi))*100,
+        'valNullEmailR3' : (valNullEmailR3/len(dataR3))*100,
+        'valNullEmailBocconi' : (valNullEmailBocconi/len(dataBocconi))*100,
+        'valNullCorsiR3' : (valNullCorsiR3/len(dataR3))*100,
+        'valNullCorsiBocconi' : (valNullCorsiBocconi/len(dataBocconi))*100,
+        }
+
+    dimensioniDataset = {
+        'numDocentiR3' : len(dataR3),
+        'numDocentiBocconi' : len(dataBocconi),
+        }
+
+    for i in dimensioniDataset:
+        print(i, dimensioniDataset[i])
+    
+    for i in caratteristicheBocconi:
+        print(i, caratteristicheBocconi[i])
+
+    for i in caratteristicheR3:
+        print(i, caratteristicheR3[i])
+
+    for i in percentualiValoriNulli:
+        print(i, percentualiValoriNulli[i])
+
+    #print(dimensioniDataset)
+    #print(percentualiValoriNulli)
+    #print(caratteristicheBocconi)
+    #print(caratteristicheR3)
